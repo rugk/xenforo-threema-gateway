94481424eeda0c88b59c017e7f61b31eb7484dbd
rugk <rugk@posteo.de>
2016-10-10 14:35:51 +0200

    Auto-move download dir if option is changed

--- src/library/ThreemaGateway/Option/DownloadPath.php
+++ src/library/ThreemaGateway/Option/DownloadPath.php
@@ -26,16 +26,60 @@ class ThreemaGateway_Option_DownloadPath
             $dirpath = substr($dirpath, 1);
         }
 
-        // check path
+        // if disabled (path = empty), we accept this
+        if ($dirpath == '') {
+            return true;
+        }
+
         $absoluteDir = XenForo_Application::getInstance()->getRootDir() . '/' . $dirpath;
-        var_dump($absoluteDir);
-        if ($dirpath != '' &&
-            !ThreemaGateway_Handler_Validation::checkDir($absoluteDir)
-        ) {
+
+        // try to move the old dir to the new place
+        try {
+            self::moveDir($absoluteDir);
+        } catch (Exception $e) {
+            // ignore me, moving the dir is not critical
+        }
+
+        // check path
+        if (!ThreemaGateway_Handler_Validation::checkDir($absoluteDir)) {
             $dw->error(new XenForo_Phrase('threemagw_invalid_downloadpath'), $fieldName);
             return false;
         }
 
         return true;
+    }
+
+
+    /**
+     * Moves old dir to new path if useful/possible.
+     *
+     * When the dir was not moved for some reason, it returns false. If a move
+     * was done, it returns true. This means it silently fails, if the dir could
+     * not be moved.
+     *
+     * @param string $newDir Please pass the absolute path here!
+     * @throws Exception (from rename())
+     * @return bool
+     */
+    protected static function moveDir($newDir)
+    {
+        /* @var XenForo_Options */
+        $xenOptions = XenForo_Application::getOptions();
+        // get absolute old dir
+        $orgDir = XenForo_Application::getInstance()->getRootDir() . '/' . $xenOptions->threema_gateway_downloadpath;
+
+        // if no option set or previous option does not apply anymore
+        // there is nothing to do
+        if (empty($xenOptions->threema_gateway_downloadpath) ||
+            !file_exists($orgDir)) {
+            return false;
+        }
+
+        // if old dir check fails, do not go on
+        if (!ThreemaGateway_Handler_Validation::checkDir($orgDir)) {
+            return false;
+        }
+
+        return rename($orgDir, $newDir);
     }
 }
